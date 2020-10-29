c191779d5d439ea98e75dcdc05a6d78a97af4c0b
zakattack9 <zakattack26111@gmail.com>
2020-10-29 00:08:29 -1000

    implemented producer, consumer, P, and V

--- Homework/Homework3/proj-3.c
+++ Homework/Homework3/proj-3.c
@@ -4,13 +4,18 @@
 #include <unistd.h>
 #include "sem.h"
 
+int buffer;
+int in, out;
+
+int BUFFER_SIZE;
+int NUM_PRODUCERS;
+int NUM_CONSUMERS;
+int NUM_TIMES_TO_RUN;
+
 int main() {
   RunQ = InitQueue(); // init queue in threads.h
-  int BUFFER_SIZE;
-  int NUM_PRODUCERS;
-  int NUM_CONSUMERS;
-  int NUM_TIMES_TO_RUN;
   
+
   Sem* full = mallocSem(); // tracks if buffer is full
   Sem* empty = mallocSem(); // tracks if buffer is empty
 
@@ -18,4 +23,20 @@ int main() {
   InitSem(empty, BUFFER_SIZE);
 
   return 0;
+}
+
+void producer(Sem *full, Sem *empty) {
+  P(empty);
+  // buffer[in] = 
+  in = ++in % BUFFER_SIZE;
+  V(full);
+  yield();
+}
+
+void consumer(Sem *full, Sem *empty) {
+  P(full);
+  // int item = buffer[out];
+  out = ++out % BUFFER_SIZE;
+  V(empty);
+  yield();
 }
--- Homework/Homework3/sem.h
+++ Homework/Homework3/sem.h
@@ -2,7 +2,7 @@
 
 typedef struct Sem {
   int val;
-  Queue* runQ;
+  Queue* SemQ; // holds blocked threads
 } Sem;
 
 Sem* mallocSem() {
@@ -14,14 +14,21 @@ void InitSem(Sem* semaphore, int value) {
 }
 
 void P(Sem* semaphore) {
-  if (semaphore->val > 0) {
-    semaphore->val--;
-    return;
-  } else {
-
-  }
+    if (semaphore->val == 0) {
+      TCB_t* item = DeleteQueue(RunQ);
+      AddQueue(semaphore->SemQ, item);
+      yield(); // or use swap context
+      // use while loop to keep checking semaphore value
+      // while (1) {
+      // }
+    } else {
+      semaphore->val--;
+      return;
+    }
 }
 
 void V(Sem* semaphore) {
   semaphore->val++;
+  TCB_t* item = DeleteQueue(semaphore->SemQ);
+  AddQueue(RunQ, item);
 }
